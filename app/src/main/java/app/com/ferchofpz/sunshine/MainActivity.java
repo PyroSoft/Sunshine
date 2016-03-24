4ca185bec7f8b00a8ad1e75623052812015c15fc
ferchofpz <ferchofpz@hotmail.com>
2016-03-23 20:58:12 -0500

    Lesson 4: starter code.

--- app/src/main/java/app/com/ferchofpz/sunshine/MainActivity.java
+++ app/src/main/java/app/com/ferchofpz/sunshine/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
